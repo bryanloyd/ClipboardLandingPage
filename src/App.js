9bbb9340b4b276c4fb1658d7dbd750e19adc9175
Bryan Loyd <bryan.loyd@gmail.com>
2021-10-04 19:08:57 -0500

    Snippets Component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
 import Header from "./Components/Header";
 import "./App.css";
+import Snippets from "./Components/Snippets";
 
 const App = () => {
   return (
     <div className="app">
       <Header />
+      <Snippets />
     </div>
   );
 };
---
+++ src/Components/Snippets.js
@@ -0,0 +1,38 @@
+import React from "react";
+import imac from "../images/image-computer.png";
+
+const Snippets = () => {
+  return (
+    <div className="snippets">
+      <div>
+        <h4>Keep track of your snippets</h4>
+        <p>
+          Clipboard instantly stores any item you copy in the cloud, meaning you
+          can access your snippets immediately on all your devices. Our Mac and
+          iOS apps will help you organize everything.
+        </p>
+      </div>
+      <section>
+        <div>
+          <img src={imac} alt="" />
+        </div>
+        <div>
+          <h5>Quick Search</h5>
+          <p>
+            Easily search your snippets by content, category, web address,
+            application, and more.
+          </p>
+          <h5>iCloud Sync</h5>
+          <p>Instantly saves and syncs snippets across all your devices.</p>
+          <h5>Complete History</h5>
+          <p>
+            Retrieve any snippets from the first moment you started using the
+            app.
+          </p>
+        </div>
+      </section>
+    </div>
+  );
+};
+
+export default Snippets;
